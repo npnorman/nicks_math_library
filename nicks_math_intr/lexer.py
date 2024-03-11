807eeb554f0103c1eeb8ad1c3218562ec5c1f297
theblindkarp <97370227+theblindkarp@users.noreply.github.com>
2024-03-10 21:05:21 -0400

    added infix to postfix in lexer

--- nicks_math_intr/lexer.py
+++ nicks_math_intr/lexer.py
@@ -9,11 +9,87 @@ class Lexer():
         pass
     
     @staticmethod
-    def infixToPostfix(eqn):
-        #use shunting yard algorithm
+    def _infixToPostfix(tokens):
+        #use shunting yard algorithm  **p.s. thanks edwin
+        #pulled from wikipedia
+        #tokens are of type Token()
 
+        print("changing order")
+        #output & stack
+        output = []
+        operatorStack = []
 
-        return eqn
+        #while there are tokens to be read:
+        for tok in tokens:
+            if (tok.type == "NUM" or tok.type == "VAR"):
+                #if a number, put in output queue
+                output.append(tok)
+
+            elif(tok.type == "FUNC"):
+                #if it is a function (NOT IMPLEMENTED)
+                #push to operator stack
+                pass
+
+            elif (tok.type == "LPAR"):
+                #left parenthesis
+                #push it to operator stack
+                operatorStack.append(tok)
+
+            elif (tok.type == "RPAR"):
+                #right parenthesis
+                #while operator at top of stack is NOT a left parentheses
+                while (operatorStack[-1].type != "LPAR"):
+                    #make sure stack isnt empty
+                    if (len(operatorStack) == 0):
+                        print("Mismatched ( or )")
+                    else:
+                        #pop the operator from stack into output queue
+                        output.append(operatorStack.pop())
+                
+                #make sure there is left parenthesis at top
+                if (operatorStack[-1].type == "LPAR"):
+                    operatorStack.pop()
+                
+                #if there is a function operator at the top
+                if (len(operatorStack) != 0):
+                    if (operatorStack[-1].type == "FUNC"):
+                        #pop it and put to output queue
+                        output.append(operatorStack.pop())
+            
+            else:
+                #operator
+                while (Lexer()._operatorCheck(operatorStack, tok) == True):
+                    #pop stack operator to output
+                    output.append(operatorStack.pop())
+                operatorStack.append(tok)
+        
+        #after loop
+        while (len(operatorStack) != 0):
+            #if operator is ( or ), mismatched
+            if (operatorStack[-1] == "(" or operatorStack[-1] == "-1"):
+                print("mismatched ( or )")
+            else:
+                output.append(operatorStack.pop())
+
+        return output
+
+    @staticmethod
+    def _operatorCheck(operatorStack, tok):
+        #check for operator in while loop
+
+        #precedence
+        precedence = {"PLUS": 2, "SUB": 2, "MULT": 3, "DIV": 3, "EXP": 4}
+        
+        output = False
+        #if operator stack is not empty
+        if (len(operatorStack) != 0):
+            #if not a left parenthesis
+            if (operatorStack[-1].type != "LPAR"):
+                #if operator on stack has greater or equal precedence
+                if(precedence[operatorStack[-1].type] >= precedence[tok.type]):
+                    output = True
+        
+        return output
 
     @staticmethod
     def eqToTokens(equation):
@@ -25,7 +101,7 @@ class Lexer():
         tokens = []
 
         #remove whitespace
-        eq = Lexer().infixToPostfix(equation)
+        eq = equation
         eq = eq.strip()
         eq = eq.replace(' ', '')
 
@@ -54,7 +130,9 @@ class Lexer():
             tempNum = ""
             tokens.append(tempToken)
 
-        return tokens
+
+        #convert to postfix
+        return Lexer()._infixToPostfix(tokens)
 
     @staticmethod
     def _convertNumberToken(str):
@@ -108,6 +186,8 @@ class Lexer():
 #testing
 if __name__ == "__main__":
     lex = "5 + 3.5 * x - 4 / 76 + (0.2 + 4) + 5.8"
+    lex = "5 + 3.5 * (2 - 7)"
+    #lex = "5 + (3 - 3)"
     print(lex)
     print("converting")
     tokens = Lexer.eqToTokens(lex)
