a2bbbdaf63a76b4014788734f989035ca9ebc1e2
theblindkarp <97370227+theblindkarp@users.noreply.github.com>
2024-03-10 22:19:47 -0400

    moved postfix from lexer to parser

--- nicks_math_intr/lexer.py
+++ nicks_math_intr/lexer.py
@@ -7,89 +7,6 @@ import token as token
 class Lexer():
     def __init__(self):
         pass
-    
-    @staticmethod
-    def _infixToPostfix(tokens):
-        #use shunting yard algorithm  **p.s. thanks edwin
-        #pulled from wikipedia
-        #tokens are of type Token()
-
-        print("changing order")
-        #output & stack
-        output = []
-        operatorStack = []
-
-        #while there are tokens to be read:
-        for tok in tokens:
-            if (tok.type == "NUM" or tok.type == "VAR"):
-                #if a number, put in output queue
-                output.append(tok)
-
-            elif(tok.type == "FUNC"):
-                #if it is a function (NOT IMPLEMENTED)
-                #push to operator stack
-                pass
-
-            elif (tok.type == "LPAR"):
-                #left parenthesis
-                #push it to operator stack
-                operatorStack.append(tok)
-
-            elif (tok.type == "RPAR"):
-                #right parenthesis
-                #while operator at top of stack is NOT a left parentheses
-                while (operatorStack[-1].type != "LPAR"):
-                    #make sure stack isnt empty
-                    if (len(operatorStack) == 0):
-                        print("Mismatched ( or )")
-                    else:
-                        #pop the operator from stack into output queue
-                        output.append(operatorStack.pop())
-                
-                #make sure there is left parenthesis at top
-                if (operatorStack[-1].type == "LPAR"):
-                    operatorStack.pop()
-                
-                #if there is a function operator at the top
-                if (len(operatorStack) != 0):
-                    if (operatorStack[-1].type == "FUNC"):
-                        #pop it and put to output queue
-                        output.append(operatorStack.pop())
-            
-            else:
-                #operator
-                while (Lexer()._operatorCheck(operatorStack, tok) == True):
-                    #pop stack operator to output
-                    output.append(operatorStack.pop())
-                operatorStack.append(tok)
-        
-        #after loop
-        while (len(operatorStack) != 0):
-            #if operator is ( or ), mismatched
-            if (operatorStack[-1] == "(" or operatorStack[-1] == "-1"):
-                print("mismatched ( or )")
-            else:
-                output.append(operatorStack.pop())
-
-        return output
-
-    @staticmethod
-    def _operatorCheck(operatorStack, tok):
-        #check for operator in while loop
-
-        #precedence
-        precedence = {"PLUS": 2, "SUB": 2, "MULT": 3, "DIV": 3, "EXP": 4}
-        
-        output = False
-        #if operator stack is not empty
-        if (len(operatorStack) != 0):
-            #if not a left parenthesis
-            if (operatorStack[-1].type != "LPAR"):
-                #if operator on stack has greater or equal precedence
-                if(precedence[operatorStack[-1].type] >= precedence[tok.type]):
-                    output = True
-        
-        return output
 
     @staticmethod
     def eqToTokens(equation):
@@ -132,7 +49,7 @@ class Lexer():
 
 
         #convert to postfix
-        return Lexer()._infixToPostfix(tokens)
+        return tokens
 
     @staticmethod
     def _convertNumberToken(str):
